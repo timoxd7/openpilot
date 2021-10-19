1fbf57379d614a494222c4943e1447d49ad599eb
timoxd7 <timoxd7@gmx.de>
2021-10-19 19:45:46 +0200

    Enable with CC, but not disable with CC

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -248,7 +248,10 @@ class CarState(CarStateBase):
 
     # Update ACC radar status.
     #ret.cruiseState.available = bool(pt_cp.vl["GRA_Neu"]['GRA_Hauptschalt'])
-    ret.cruiseState.enabled = True  # if pt_cp.vl["Motor_2"]['GRA_Status'] in [1, 2] else False
+
+    if pt_cp.vl["Motor_2"]['GRA_Status'] in [1, 2]:
+      ret.cruiseState.enabled = True # -> Disable only over brake, enable only over CC
+
     ret.cruiseState.available = ret.cruiseState.enabled  # Test hax for e-up!
 
     # Update ACC setpoint. When the setpoint reads as 255, the driver has not
